cc350dede64022e5f7991464cf046aac0c94ed36
youssefberrk <yberrakouan.lol@gmail.com>
2023-11-30 03:35:16 -0800

    DONEIT

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for text_indentation method."""
 
+
 def text_indentation(text):
     """Prints text with two new lines after each '.', '?', and ':'.
 
@@ -29,6 +30,7 @@ def text_indentation(text):
                 start += 1
             continue
         start += 1
+
 
 if __name__ == "__main__":
     try:
