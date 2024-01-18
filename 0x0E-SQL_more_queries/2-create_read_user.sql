1f7095337c905fc9de195d396437c85c0181096e
youssefberrk <yberrakouan.lol@gmail.com>
2024-01-18 05:32:34 -0800

    DONEIT

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2.
--- create database
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
---create user wit pwd
+-- creates a user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grant privilege
+-- grants SELECT privileges to a user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
